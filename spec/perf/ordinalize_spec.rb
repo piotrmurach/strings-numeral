a8352ae733f1957cbd819f1a56a60c6216e66136
Piotr Murach <pmurach@gmail.com>
2019-12-14 18:45:38 +0100

    Add comparison with active support ordinalize

---
+++ spec/perf/ordinalize_spec.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require "rspec-benchmark"
+require "active_support"
+
+RSpec.describe Strings::Numeral do
+  include RSpec::Benchmark::Matchers
+
+  it "converts number to short ordinal faster than ActiveSupport" do
+    expect {
+      Strings::Numeral.ordinalize(12345)
+    }.to perform_faster_than {
+      ActiveSupport::Inflector.ordinalize(12345)
+    }.at_least(500).times
+  end
+end
