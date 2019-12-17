df4bf6fbb79081bc289917db42670e5d61575023
Piotr Murach <pmurach@gmail.com>
2019-12-17 20:43:22 +0100

    Add perf test for regular ordinalize

--- spec/perf/ordinalize_spec.rb
+++ spec/perf/ordinalize_spec.rb
@@ -8,9 +8,17 @@ RSpec.describe Strings::Numeral do
 
   it "converts number to short ordinal faster than ActiveSupport" do
     expect {
-      Strings::Numeral.ordinalize(12345, short: true)
+      Strings::Numeral.ordinalize_short(12345)
     }.to perform_faster_than {
       ActiveSupport::Inflector.ordinalize(12345)
-    }.at_least(400).times
+    }.at_least(350).times
+  end
+
+  it "converts number to ordinal 10 times faster than ActiveSupport" do
+    expect {
+      Strings::Numeral.ordinalize(12345)
+    }.to perform_faster_than {
+      ActiveSupport::Inflector.ordinalize(12345)
+    }.at_least(10).times
   end
 end
